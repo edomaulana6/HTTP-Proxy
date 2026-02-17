092864c161825f34f44d0fa18beeb2eb4080e356
Leonel Macejkovic <edom70976@gmail.com>
2026-02-17 13:45:51 +0700

    Add script to download and run V2Ray

---
+++ start.sh
@@ -0,0 +1,11 @@
+# 1. Unduh Core V2Ray Portable
+curl -L -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip [cite: 2025-12-27]
+
+# 2. Ekstrak file
+unzip v2ray.zip [cite: 2025-12-27]
+
+# 3. Beri izin eksekusi
+chmod +x v2ray [cite: 2025-12-27]
+
+# 4. Jalankan dengan config di atas
+./v2ray run -c config.json [cite: 2026-02-17]
